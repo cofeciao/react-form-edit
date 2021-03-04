2bdf98d0248b2232cc5ede4ab2f4a4ded5a80e7d
Cao Minh <trancaominh1997@gmail.com>
2021-03-04 17:51:06 +0700

    store.js update

--- src/Store.js
+++ src/Store.js
@@ -1,4 +1,6 @@
-import
+import numReducer from './reducer/numReducer';
+import isStatusReducer from "./reducer/isStatusReducer";
+
 var redux = require('redux');
 var oldState = {
     num : ['alo' , 'cái' , 'xô'],
@@ -18,30 +20,37 @@ var reducer1 = (state = oldState,action) => {
     }
 }
 
-const numInitialState = ['alo' , 'cái' , 'xô'];
-const numReducer = (state = numInitialState,action) => {
-    switch (action.type) {
-        case "ADD_NEW" :
-            return [...state,action.newItem];
-        case 'DELETE' :
-            return state.num.filter((value,index) =>
-                index !== action.index
-            );
-    }
-}
+// const numInitialState = ['alo' , 'cái' , 'xô'];
+// const numReducer = (state = numInitialState,action) => {
+//     switch (action.type) {
+//         case "ADD_NEW" :
+//             return [...state,action.newItem];
+//         case 'DELETE' :
+//             return state.num.filter((value,index) =>
+//                 index !== action.index
+//             );
+//     }
+// }
+//
+// const isStatusInitialState = ['alo' , 'cái' , 'xô'];
+// const isStatusReducer = (state = isStatusInitialState,action) => {
+//     switch (action.type) {
+//         case "CHANGE_EDIT_STATUS" :
+//             return {...state,isStatus:!state.isStatus};
+//     }
+// }
 
-const isStatusInitialState = ['alo' , 'cái' , 'xô'];
-const isStatusReducer = (state = isStatusInitialState,action) => {
-    switch (action.type) {
-        case "CHANGE_EDIT_STATUS" :
-            return {...state,isStatus:!state.isStatus};
-    }
-}
+const allReducers = redux.combineReducers({
+    num: numReducer,
+    isStatus: isStatusReducer,
+})
 
 var store1 = redux.createStore(reducer1);
 store1.subscribe(() => {
     store1.getState();
 });
+
+
 
 
 store1.dispatch({type: "CHANGE_EDIT_STATUS"});
