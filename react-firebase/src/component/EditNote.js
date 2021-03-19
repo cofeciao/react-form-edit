1d8ba57593852b0152dfeb9651a7ca247945bf75
Cao Minh <trancaominh1997@gmail.com>
2021-03-19 18:05:00 +0700

    upload code

--- react-firebase/src/component/EditNote.js
+++ react-firebase/src/component/EditNote.js
@@ -1,17 +1,37 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 class EditNote extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    getTextForm = (event) => {
+        event.preventDefault();
+        let name = event.target.name;
+        let value = event.target.value;
+        this.setState({
+            [name]: value
+        });
+    }
+
+    addNote = (event) => {
+        event.preventDefault();
+    }
+
     render() {
         return (
                 <form action="POST" className="mt-3">
                     <h3>Sửa nội dung Note</h3>
                     <div className="form-group mt-5">
                         <label className="h5" htmlFor="noteTitle">Title Note</label>
-                        <input type="text" className="form-control" name="noteTitle" aria-describedby="helpId" placeholder="điền tiêu đề vào đây" />
+                        <input onChange={(event) => {this.getTextForm(event)}} type="text" className="form-control" name="noteTitle" aria-describedby="helpId" placeholder="điền tiêu đề vào đây" />
                         <br/>
                         <label className="h5" htmlFor="noteContent">Content Note</label>
-                        <textarea  name="noteContent" id="contentNote" cols={29} rows={5} placeholder=" điền nội dung vào đây"/>
-                        <button type="submit" className="btn btn-primary btn-block mt-3">Gửi</button>
+                        <textarea onChange={(event) => {this.getTextForm(event)}} name="noteContent" id="contentNote" cols={29} rows={5} placeholder=" điền nội dung vào đây"/>
+                        <button onClick={(event) => {this.addNote()}} type="submit" className="btn btn-primary btn-block mt-3">Gửi</button>
                     </div>
                 </form>
         );
--- react-firebase/src/component/ManageNote.js
+++ react-firebase/src/component/ManageNote.js
@@ -4,6 +4,7 @@ import {connect} from 'react-redux';
 import firebase from "firebase";
 
 class ManageNote extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
@@ -13,7 +14,7 @@ class ManageNote extends Component {
 
     componentWillMount() {
         let data = firebase.database().ref('noteList/');
-        var array = [];
+        let array = [];
         data.on('value',(items) => {
             items.forEach(item => {
                 const title = item.val().title;
@@ -32,85 +33,59 @@ class ManageNote extends Component {
         })
     }
 
-    cardHeader = () => {
-        this.state.data.forEach(item => {
-        // console.log(item.category);
-        return (
-            <div className="card">
-                <div className="card-header">
-                    <a className="card-link" data-toggle="collapse" href="#note1">
-                        {/*{item.category}*/}Music
-                    </a>
-                </div>
-                <div id="note1" className="collapse show" data-parent="#noteList">
-                    llll
-                    {/*<div className="card-body">*/}
-                    {/*    <div id="noteListChild">*/}
-                    {/*        <div className="card">*/}
-                    {/*            <div className="card-header">*/}
-                    {/*                <a className="card-link" data-toggle="collapse" href="#note2">*/}
-                    {/*                    Ngày 9/3 2021*/}
-                    {/*                </a>*/}
-                    {/*            </div>*/}
-                    {/*            <div id="note2" className="collapse show" data-parent="#noteListChild">*/}
-                    {/*                <div className="card-body">*/}
-                    {/*                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor*/}
-                    {/*                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis*/}
-                    {/*                    nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.*/}
-                    {/*                </div>*/}
-                    {/*            </div>*/}
-                    {/*        </div>*/}
-                    {/*    </div>*/}
-                    {/*</div>*/}
-                </div>
-            </div>
-        )
-    });
-    }
-        // })
-
-        //     cardHeader = () => {
-        //     this.state.data.forEach((item,key) => {
-        //         console.log(item.category);
-        //         return (
-        //             <div className="card" >
-        //                 <div className="card-header">
-        //                     <a className="card-link" data-toggle="collapse" href={'#note'+key}>
-        //                         {item.category}
-        //                     </a>
-        //                 </div>
-        //                 <div id={'note'+key} className="collapse show" data-parent="#noteList">
-        //                     llll
-        //                     {/*<div className="card-body">*/}
-        //                     {/*    <div id="noteListChild">*/}
-        //                     {/*        <div className="card">*/}
-        //                     {/*            <div className="card-header">*/}
-        //                     {/*                <a className="card-link" data-toggle="collapse" href="#note2">*/}
-        //                     {/*                    Ngày 9/3 2021*/}
-        //                     {/*                </a>*/}
-        //                     {/*            </div>*/}
-        //                     {/*            <div id="note2" className="collapse show" data-parent="#noteListChild">*/}
-        //                     {/*                <div className="card-body">*/}
-        //                     {/*                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.*/}
-        //                     {/*                </div>*/}
-        //                     {/*            </div>*/}
-        //                     {/*        </div>*/}
-        //                     {/*    </div>*/}
-        //                     {/*</div>*/}
-        //                 </div>
-        //             </div>
-        //         )
-        //     })
-        // }
+    cardHeader = () =>
+        this.state.data.map((item,key) => {
+                return (
+                    // <div className="card">
+                    //     <div className="card-header">
+                    //         <a className="card-link" data-toggle="collapse" href={"#nnote"+key}>
+                    //             {item.category}
+                    //         </a>
+                    //     </div>
+                    //     <div id={"nnote"+key} className="collapse show" data-parent="#noteList">
+                    //         <div className="card-body">
+                    <form>
+                        <div id="noteListChild" key={key}>
+                            <div className="card">
+                                <div className="card-header">
+                                    <div className="row">
+                                        <div className="col">
+                                            <a className="card-link" data-toggle="collapse" href={"#note"+key}>
+                                                {item.title}
+                                            </a>
+                                        </div>
+                                        <div className="col-3">
+                                            <div className="row ml-5">
+                                                    <div className="btn btn-warning">Sửa</div>
+                                                    <div className="btn btn-danger">Xóa</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div id={"note"+key} className="collapse show" data-parent="#noteListChild">
+                                    <div className="card-body">
+                                        {item.content}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                            // </div>
+                        // </div>
+                    // </div>
+                )
+        });
 
     render() {
-        // console.log(JSON.stringify(this.cardHeader))
+
         return (
             <div className="col mt-3">
                 <h2>Quản Lý Note</h2>
                 <p><strong>Note:</strong> The <strong>data-parent</strong> attribute makes sure that all collapsible elements under the specified parent will be closed when one of the collapsible item is shown.</p>
                 <div id="noteList">
-                    {this.cardHeader()}
+                    {
+                        this.cardHeader()
+                    }
                 </div>
             </div>
         );
